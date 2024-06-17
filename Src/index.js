dd64a2ff37e9ef3e5eb2d24943d7dd5176ef8847
Hanine <91432774+hanine007@users.noreply.github.com>
2024-06-17 11:10:27 +0000

    changes

--- Src/index.js
+++ Src/index.js
@@ -1,7 +1,7 @@
 const http = require ('http')
 const server =http.createServer (async (req,res)=>{
     if (req.methode ==='GET'&& req.URL==='/')
-
+console.log ('hello from the server')
 res.end()
 })
 server.listen (3001, ()=>{
