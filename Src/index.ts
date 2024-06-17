19e9b5db47379e7f05694b68f9e292d21f1be454
Hanine <91432774+hanine007@users.noreply.github.com>
2024-06-17 22:13:55 +0000

    add

---
+++ Src/index.ts
@@ -0,0 +1,20 @@
+/* using http module
+const http = require('http');
+const server = http.createServer(async (req, res) => {
+    if (req.method === 'GET' && req.url === '/')
+        console.log('hello from the server')
+    res.end()
+})
+server.listen(3001, () => {
+    console.log('Server on http://localhost:3001  ')
+})
+    */
+
+//express js
+   
+   
+//const app = require('./server');
+import app from './server'
+app.listen(3001, () => {
+    console.log('hello from the server on 3001');
+});
