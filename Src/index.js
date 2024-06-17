5971789a520256dbab330d2df386c52edda08e8c
Hanine <91432774+hanine007@users.noreply.github.com>
2024-06-17 11:25:07 +0000

    delete the e in the methode syntax mdr

--- Src/index.js
+++ Src/index.js
@@ -1,9 +1,9 @@
-const http = require ('http')
-const server =http.createServer (async (req,res)=>{
-    if (req.methode ==='GET'&& req.URL==='/')
-console.log ('hello from the server')
-res.end()
+const http = require('http');
+const server = http.createServer(async (req, res) => {
+    if (req.method === 'GET' && req.url === '/')
+        console.log('hello from the server')
+    res.end()
 })
-server.listen (3001, ()=>{
-    console.log('Server on local Run  ')
+server.listen(3001, () => {
+    console.log('Server on http://localhost:3001  ')
 })
