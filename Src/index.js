0c63fd25d7be28de6bff0c3d6f8c0a6a940d63f5
Hanine <91432774+hanine007@users.noreply.github.com>
2024-06-17 13:16:38 +0000

    Add a express app

--- Src/index.js
+++ Src/index.js
@@ -1,3 +1,4 @@
+/* using http module
 const http = require('http');
 const server = http.createServer(async (req, res) => {
     if (req.method === 'GET' && req.url === '/')
@@ -7,3 +8,13 @@ const server = http.createServer(async (req, res) => {
 server.listen(3001, () => {
     console.log('Server on http://localhost:3001  ')
 })
+    */
+
+//express js
+   
+   
+const app = require('./server');
+
+app.listen(3001, () => {
+    console.log('hello from the server on 3001');
+});
