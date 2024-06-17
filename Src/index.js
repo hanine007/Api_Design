bceaa23769dcf05d2198dc142082b1c47d35069c
Hanine <91432774+hanine007@users.noreply.github.com>
2024-06-17 11:00:34 +0000

    modify http

--- Src/index.js
+++ Src/index.js
@@ -1,8 +1,9 @@
-const http = require ('HTTP')
+const http = require ('http')
 const server =http.createServer (async (req,res)=>{
     if (req.methode ==='GET'&& req.URL==='/')
 res.statusCode(200)
 res.end()
-
-
+})
+server.listen (3001, ()=>{
+    console.log('Server on local Run  ')
 })
