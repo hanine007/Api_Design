017fddb75305941f0661c64babb4556707fb7786
Hanine <91432774+hanine007@users.noreply.github.com>
2024-06-18 11:41:49 +0000

    import router

--- Src/server.ts
+++ Src/server.ts
@@ -1,5 +1,8 @@
 //const express= require('express');
+
 import express from 'express';
+import router from './route';
+
 const app = express()
 app.get('/',(req,res)=>{
     console.log('hello fromm the express app ')
@@ -7,6 +10,14 @@ app.get('/',(req,res)=>{
    // res.json({message:'hanine'})
     res.send('Hello World');
 })
+
+app.use('/api',router)
+
+
+
+
+
+
 
 
 //module.exports = app;
