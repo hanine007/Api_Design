8be44aef951b3c1750ad9f18cc8c813cd1f322d3
Hanine <91432774+hanine007@users.noreply.github.com>
2024-06-19 23:46:18 +0000

    catch

--- Src/modules/auth.ts
+++ Src/modules/auth.ts
@@ -1,6 +1,6 @@
 import jwt from 'jsonwebtoken'
 export const Createjwt=(user)=>{
-    
+
     const token =jwt.sign ({
         id:user.id,
         username:user.username
@@ -9,19 +9,31 @@ export const Createjwt=(user)=>{
         process.env.JWT_SECRET)
         return token
 } 
-export const protect=(req,res)=>{
+export const protect=(req,res,next)=>{
     //check if they have the authorzation 
     const bearer= req.headers.authorization;
     if(!bearer){
         res.status(401)
         res.json({message:'Not authorized'})}
 
-//verify the token if is valide
+//verify the token if's exist
         const [,token]=bearer.split (' ')
         if(!token ){
             res.status(401)
         res.json({message:'Invalide Token'})
         }
-
+//verify the token 
+try{
+const payload =jwt.verify(token ,process.env.JWT_SECRET)
+req.user=payload
+console.log(payload);
+next()
+}
+catch(e){
+    //get information about the error
+    console.error(e)
+    res.status(401)
+    res.json({message:'invalid'})
+}
 
 }
