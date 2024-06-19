58bec2a8ac7b53bde0a964de6404e1d45ef39364
Hanine <91432774+hanine007@users.noreply.github.com>
2024-06-19 22:27:38 +0000

    jwt

---
+++ Src/modules/auth.ts
@@ -0,0 +1,9 @@
+import jwt from 'jsonwebtoken'
+export const Createjwt=(user)=>{
+    const token =jwt.sign ({
+        id:user.id,
+        username:user.username
+
+    },
+        process.env.JWT_SECRET)
+} 
