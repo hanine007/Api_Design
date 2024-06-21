86c0d2589c0bedcec513f3ce0a38d287fdb8e1d7
Hanine <91432774+hanine007@users.noreply.github.com>
2024-06-21 11:27:01 +0000

    Signin fct

--- Src/handlers/user.ts
+++ Src/handlers/user.ts
@@ -1,6 +1,7 @@
 import { create } from 'domain'
 import prisma from '../db'
-import { Createjwt, hashPassword } from '../modules/auth'
+import { Createjwt, comparePassword, hashPassword } from '../modules/auth'
+//Create a user
 export const CreateNewUser= async(req,res)=>{
 const user =await prisma.user.create({
     data:{
@@ -12,4 +13,21 @@ const user =await prisma.user.create({
 //assign a token a new user create
 const token = Createjwt(user)
 res.json ({token})
+}
+//Signin  A user
+export const signin =async(req,res)=>{
+    const user = await prisma.user.findUnique({
+        where:{
+            Usernmae:req.body.Username
+        }
+    })
+ const isValid = await comparePassword(req.body.Password,hashPassword)
+ if (!isValid){
+    res.status(401)
+    res.json({message:'nooo'})
+ return}
+
+const token=Createjwt(user)
+res.json({token})
+
 }
