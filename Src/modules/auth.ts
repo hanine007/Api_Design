2925611c940c781c6c8746202ef33e62455b4cef
Hanine <91432774+hanine007@users.noreply.github.com>
2024-06-21 10:31:21 +0000

    add comparepassword function

--- Src/modules/auth.ts
+++ Src/modules/auth.ts
@@ -1,9 +1,19 @@
 import jwt from 'jsonwebtoken'
-export const Createjwt=(user)=>{
+import bcrypt from 'bcrypt'
+
+export const comparePassword =(Password,hash)=>{
+    return bcrypt.compare(Password,hash)
+}
+export const hashPassword =(Password)=>{
+    return bcrypt.hash(Password,5)
+}
+
+
+export const Createjwt=(User)=>{
 
     const token =jwt.sign ({
-        id:user.id,
-        username:user.username
+        Id:User.Id,
+        Username:User.Username
 
     },
         process.env.JWT_SECRET)
@@ -14,14 +24,16 @@ export const protect=(req,res,next)=>{
     const bearer= req.headers.authorization;
     if(!bearer){
         res.status(401)
-        res.json({message:'Not authorized'})}
+        res.json({message:'Not authorized'})
+    return}
 
 //verify the token if's exist
         const [,token]=bearer.split (' ')
         if(!token ){
             res.status(401)
         res.json({message:'Invalide Token'})
-        }
+        return
+            }
 //verify the token 
     try{
 const payload =jwt.verify(token ,process.env.JWT_SECRET)
