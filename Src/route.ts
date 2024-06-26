54e1106af006d5f2bce93f1c99c13d04d34b748b
Hanine <91432774+hanine007@users.noreply.github.com>
2024-06-26 10:33:19 +0000

    Input validation

--- Src/route.ts
+++ Src/route.ts
@@ -1,5 +1,7 @@
 import {Router}from 'express'
 import {body, validationResult} from 'express-validator'
+import { handleInputEroor } from './modules/handdler'
+import { version } from 'os'
 const router =Router()
 //Product 
 router.get ('/product',(req,res)=>{
@@ -10,15 +12,10 @@ router.get('/product/:id',(req,res)=>{
 
 }) 
 //modify product
-router.put ('/product/:id',body("name"),(req,res)=>{
-    //validationResult contain all the error validation 
-const errors= validationResult(req)
-if (!errors.isEmpty()){
-    res.status (400)
-    res.json({error:errors.array()})
-}
+router.put ('/product/:id',body("name"),handleInputEroor,(req,res)=>{
+
 })
-router.post('/product',(req,res)=>{
+router.post('/product',body("name"),handleInputEroor,(req,res)=>{
 
 })
 router.delete('/product/:id',(req,res)=>{
@@ -29,9 +26,14 @@ router.get("/update", (req, res) => {});
 
 router.get("/update/:id", (req, res) => {});
 
-router.post("/update", (req, res) => {});
+router.post("/update", 
+body("title").optional,
+body("body").optional,
+body("status").optional,
+body("version").optional,
+(req, res) => {});
 
-router.put("/update/:id", (req, res) => {});
+router.put("/update/:id", handleInputEroor,(req, res) => {});
 
 router.delete("/update/:id", (req, res) => {});
 
@@ -41,9 +43,9 @@ router.get("/updatepoint", (req, res) => {});
 
 router.get("/updatepoint/:id", (req, res) => {});
 
-router.post("/updatepoint", (req, res) => {});
+router.post("/updatepoint", handleInputEroor,(req, res) => {});
 
-router.put("/updatepoint/:id", (req, res) => {});
+router.put("/updatepoint/:id", handleInputEroor,(req, res) => {});
 
 router.delete("/updatepoint/:id", (req, res) => {});
 
