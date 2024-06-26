fa0e05625c625755cf31d812d3498c8968c96ade
Hanine <91432774+hanine007@users.noreply.github.com>
2024-06-26 10:32:38 +0000

    Add a comment

--- Src/modules/handdler.ts
+++ Src/modules/handdler.ts
@@ -1,3 +1,12 @@
+import { validationResult } from "express-validator"
 export const handleInputEroor=(req,res,next)=>{
-    
+    //validation error conatain all the error validation 
+    const errors= validationResult(req)
+if (!errors.isEmpty()){
+    res.status (400)
+    res.json({error:errors.array()})
+}
+else {
+next()
+}
 }
