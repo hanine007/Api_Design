71552b82bc2c1ea95512006bc45d9c7d8f801d09
Hanine <91432774+hanine007@users.noreply.github.com>
2024-06-30 14:30:49 +0000

    get all updates

--- Src/handlers/update.ts
+++ Src/handlers/update.ts
@@ -1,4 +1,5 @@
 import prisma from "../db";
+//getone update
 export const getOneupdate = async(req,res)=>{
     const update = await prisma.update.findUnique({
         where:{
@@ -7,3 +8,21 @@ export const getOneupdate = async(req,res)=>{
         })
         res.json ({data:update})
     }
+
+//get all update
+export const getUpdates=async (req,res)=>{
+    const products= await prisma.product.findMany({
+        where:{
+            belongsToId:req.user.Id
+
+        },
+        include:{
+            Update:true
+        }
+    })
+    const updates = products.reduce((allUpdates, product) => {
+        return [...allUpdates, ...product.Update]
+      }, [])
+    
+      res.json({data: updates})
+}
