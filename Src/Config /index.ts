ff2087a287b9b33cd3d4f68e18975ca7a2f05f2b
Hanine <91432774+hanine007@users.noreply.github.com>
2024-07-01 20:07:01 +0000

    environments

--- Src/Config /index.ts
+++ Src/Config /index.ts
@@ -1,9 +1,25 @@
 import merge from 'lodash.merge'
 process.env.NODE_ENV= process.env.NODE_ENV ||'development ';
-const stage= process.env.STAGE||'local ';
+const stage= process.env.STAGE||'local '; 
 let envConf
 if (stage==='production')
     {
-envConf= require('./prod')
+envConf= require('./prod').default
     }
-    
+    else if(stage==='testing') {
+        envConf= require('./testing')
+    }
+    else {
+        envConf=require('./local')
+    }
+
+    export default merge({
+        //merge two object the base object and the envconf 
+        stage,
+        env:process.env.NODE_ENV,
+        port:3005,
+        secrets:{
+            jwt :process.env.JWT_SECRET,
+            dburl:process.env.DATABASE_URL
+        }
+    },envConf)
